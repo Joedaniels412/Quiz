079094038f657b30c228d2117c4eaa3221e08b02
Joedaniels412 <106490067+Joedaniels412@users.noreply.github.com>
2022-08-10 19:46:02 +0100

    Quiz

--- main.py
+++ main.py
@@ -18,19 +18,17 @@ print("E - " + (quest1[4]))
 print("")
 question1 = input("Which one of these graphics cards is the most powerful?\n")
 
-while True:
-    if question1 == "E":
-        print("Correct!\n")
-        score = score + 1
-    elif question1 == "e":
-        print("Correct!\n")
-        score = score + 1
-    elif question1 == "3080":
-        print("Correct!\n")
-        score = score + 1
-    else:
-        print("Incorrect\n")
-    break
+if question1 == "E":
+    print("Correct!\n")
+    score = score + 1
+elif question1 == "e":
+    print("Correct!\n")
+    score = score + 1
+elif question1 == "3080":
+    print("Correct!\n")
+    score = score + 1
+else:
+    print("Incorrect\n")
 
 print("A - " + (quest2[0]))
 print("B - " + (quest2[1]))
@@ -41,19 +39,17 @@ print("")
 
 question2 = input("What is the largest passenger aircraft in the world?\n")
 
-while True:
-    if question2 == "B":
-        print("Correct!\n")
-        score = score + 1
-    elif question2 == "b":
-        print("Correct!\n")
-        score = score + 1
-    elif question2 == "Airbus A380":
-        print("Correct!\n")
-        score = score + 1
-    else:
-        print("Incorrect\n")
-    break
+if question2 == "B":
+    print("Correct!\n")
+    score = score + 1
+elif question2 == "b":
+    print("Correct!\n")
+    score = score + 1
+elif question2 == "Airbus A380":
+    print("Correct!\n")
+    score = score + 1
+else:
+    print("Incorrect\n")
 
 print("A - " + (quest3[0]))
 print("B - " + (quest3[1]))
@@ -64,19 +60,17 @@ print("")
 
 question3 = input("What language was this quiz written in?\n")
 
-while True:
-    if question3 == "A":
-        print("Correct!\n")
-        score = score + 1
-    elif question3 == "a":
-        print("Correct!\n")
-        score = score + 1
-    elif question3 == "Python":
-        print("Correct!\n")
-        score = score + 1
-    else:
-        print("Incorrect\n")
-    break
+if question3 == "A":
+    print("Correct!\n")
+    score = score + 1
+elif question3 == "a":
+    print("Correct!\n")
+    score = score + 1
+elif question3 == "Python":
+    print("Correct!\n")
+    score = score + 1
+else:
+    print("Incorrect\n")
 
 print("A - " + (quest4[0]))
 print("B - " + (quest4[1]))
@@ -87,19 +81,17 @@ print("")
 
 question4 = input("Out of the list, which of these people co-founded Microsoft?\n")
 
-while True:
-    if question4 == "D":
-        print("Correct!\n")
-        score = score + 1
-    elif question4 == "d":
-        print("Correct!\n")
-        score = score + 1
-    elif question4 == "Bill Gates":
-        print("Correct!\n")
-        score = score + 1
-    else:
-        print("Incorrect\n")
-    break
+if question4 == "D":
+    print("Correct!\n")
+    score = score + 1
+elif question4 == "d":
+    print("Correct!\n")
+    score = score + 1
+elif question4 == "Bill Gates":
+    print("Correct!\n")
+    score = score + 1
+else:
+    print("Incorrect\n")
 
 print("A - " + (quest5[0]))
 print("B - " + (quest5[1]))
@@ -110,19 +102,17 @@ print("")
 
 question5 = input("What is the capital city of the UK?\n")
 
-while True:
-    if question5 == "B":
-        print("Correct!\n")
-        score = score + 1
-    elif question5 == "b":
-        print("Correct!\n")
-        score = score + 1
-    elif question5 == "London":
-        print("Correct!\n")
-        score = score + 1
-    else:
-        print("Incorrect\n")
-    break
+if question5 == "B":
+    print("Correct!\n")
+    score = score + 1
+elif question5 == "b":
+    print("Correct!\n")
+    score = score + 1
+elif question5 == "London":
+    print("Correct!\n")
+    score = score + 1
+else:
+    print("Incorrect\n")
 
 print(total, score)
 time.sleep(20)
